deb28c76c25584c20fc7f534c44971fee7027093
Will Kaspar <Will@Aang.local>
2017-04-28 15:41:10 -0700

    image load optimization

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,14 @@ console.log('app.js linked');
 
 $(document).ready(function(){
 
-  $('.parallax').parallax();
-  $('.scrollspy').scrollSpy();
-  $('.modal').modal();
   $('.first-bg').hide();
   $('.first-bg').fadeIn(3000, function(e){
     $('.arrow').fadeIn(1000);
     $('.arrow').removeClass('hide');
   });
+  $('.parallax').parallax();
+  $('.modal').modal();
+  $('.scrollspy').scrollSpy();
 
   $(window).on('scroll', function(e){
     $('.arrow').fadeOut(1000);
