1eb52363f7a20821092ad642b243f86b7d56a242
Will Kaspar <Will@Aang.local>
2017-04-28 15:19:37 -0700

    small changes to .ready load order

--- scripts/app.js
+++ scripts/app.js
@@ -3,18 +3,18 @@ console.log('app.js linked');
 
 $(document).ready(function(){
 
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+  $('.modal').modal();
   $('.first-bg').hide();
   $('.first-bg').fadeIn(3000, function(e){
     $('.arrow').fadeIn(1000);
     $('.arrow').removeClass('hide');
   });
 
-  $('.arrow').on('scroll', function(e){
+  $(window).on('scroll', function(e){
     $('.arrow').fadeOut(1000);
   })
 
-  $('.parallax').parallax();
-  $('.scrollspy').scrollSpy();
-  $('.modal').modal();
 
 }); // closes document on ready
