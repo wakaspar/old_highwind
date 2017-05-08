5036de3aace7bdc86c0ac1f0bb9f36a9e996e7f9
Will Kaspar <Will@Aang.local>
2017-05-08 11:27:42 -0700

    replaces background images with patterns

--- scripts/app.js
+++ scripts/app.js
@@ -3,18 +3,12 @@ console.log('app.js linked');
 
 $(document).ready(function(){
 
-  // $('.first-bg').hide();
-  $('.first-bg').fadeIn(2000, function(e){
-    $('.arrow').fadeIn(1000);
-    $('.arrow').removeClass('hide');
-  });
+
   $('.parallax').parallax();
   $('.modal').modal();
-  $('.scrollspy').scrollSpy();
 
-  $(window).on('scroll', function(e){
-    $('.arrow').fadeOut(1000);
-  })
+  $('.arrow').fadeIn(3000);
+  $('.arrow').removeClass('hide');
 
 
 }); // closes document on ready
