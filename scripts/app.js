d8d6ff91fc3d58589c4782d3f23faee9e7e259fe
Will Kaspar <Will@Aang.local>
2017-05-10 10:12:55 -0700

    fixes arrow, adds toats, adjusts layout to make things more uniform

--- scripts/app.js
+++ scripts/app.js
@@ -7,8 +7,22 @@ $(document).ready(function(){
   $('.parallax').parallax();
   $('.modal').modal();
 
-  $('.arrow').fadeIn(3000);
+  $('.arrow').fadeIn(2000);
   $('.arrow').removeClass('hide');
+
+  $(window).on('scroll', function(e){
+    $('.arrow').fadeOut(500);
+  })
+
+  var options = [
+    {selector: '#page-one', offset: 50, callback: function(el) {
+      Materialize.toast("Welcome to Will Kaspar's personal website!", 2500 );
+    } },
+    {selector: '#page-five', offset: 715, callback: function(el) {
+      Materialize.toast("Thank you for visiting. The footer icons provide more info", 2500 );
+    } }
+  ];
+  Materialize.scrollFire(options);
 
 
 }); // closes document on ready
