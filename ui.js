e71903f8f3bab586325dc22f7fbe056183680985
Will Kaspar <Will@Aang.local>
2018-03-08 18:42:09 -0800

    dropdown menu working, getting ready to implement Angular & set up controllers.  file structure will probably change more.

---
+++ ui.js
@@ -0,0 +1,14 @@
+console.log('sanity check');
+
+
+/* Navigation controls */
+function openNav() {
+  document.getElementById("nav").style.width = "100vw";
+}
+function closeNav() {
+  document.getElementById("nav").style.width = "0vw";
+}
+
+/* jQuery */
+$(document).ready(function(){
+}); // closes document.ready();
