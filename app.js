612bfcf8020f1f9829620c95c4ff6ecd8ec5f8a6
Will Kaspar <Will@Aang.local>
2018-03-08 20:35:38 -0800

    HomeController and home.html template built and implemented, attempt to turn on server and serve view to ng-view

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var app = angular
-  .module('highwind', ['ngRoute'])
+  .module('Highwind', ['ngRoute'])
   .config(config);
 
   config.$inject = ['$routeProvider', '$locationProvider'];
 
   function config($routeProvider, $locationProvider){
     console.log('angular working');
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+      })
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
   }
---
+++ controllers/HomeController.js
@@ -0,0 +1,8 @@
+angular
+  .module('Highwind')
+  .controller('HomeController', HomeController);
+
+// HomeController.$inject = ['$http'];
+function HomeController(){
+  console.log('HomeController works!');
+}
